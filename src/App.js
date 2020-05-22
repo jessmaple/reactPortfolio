ee1ce088df08a0824d8e9a7727efe3508e7288f5
Jessica Maple <maplejessbeth@gmail.com>
2020-05-22 03:06:11 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <div className="navigation-bar">
+    <div id="navigation-container">
+      <a href="index.html"><img src="./assets/logo.png" width="100"></a>
+      <ul>
+        <li><a href="about.html">about</a></li>
+        <li><a href="work.html">work</a></li>
+        <li><a href="https://github.com/jessmaple">github</a></li>
+        <li><a href="https://www.linkedin.com/in/jessmaple/">linkedin</a></li>
+        <li><a href="resume.html">resume</a></li>
+      </ul>
+    </div>
+
+    <div className="container">
+      <p className="title">
+        <h1>Hi! I'm Jessica Maple, a full-stack developer
+          <br>
+          based in Atlanta, Georgia
+        </h1>
+      </p>
+    </div>
+    <br>
+    <br>
+    <div className="footer">
+      <p>© jessica maple 2020 | All Rights Reserved</p>
+    </div>
+  </div>
+  </div>
     </div>
   );
 }
